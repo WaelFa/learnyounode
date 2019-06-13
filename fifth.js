5c37f39c534c10b773ebf6cf7568f310758ebb27
WaelFa <wael.fadhel93@gmail.com>
2019-06-13 16:08:16 +0100

    fifth

--- fifth.js
+++ fifth.js
@@ -1,3 +1,10 @@
 var fs = require('fs')
+const path =require('path')
 
-const 
+const result = fs.readdir(process.argv[2], (err, data) => {
+    if (err) throw err;
+    else {
+        console.log(data.filter(el => path.extname(el).toString()==='.'+process.argv[3] ).forEach(el=> console.log(el)))
+    }
+})
+ 
